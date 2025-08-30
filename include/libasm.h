abbc25040867f6ac210e09c444f7cf4a2efca681
jkong <jkong@student.42seoul.kr>
2025-08-30 16:30:19 +0900

    define bonus

--- include/libasm.h
+++ include/libasm.h
@@ -21,3 +21,19 @@ int ft_strcmp(const char* s1, const char* s2);
 ssize_t ft_write(int fd, const void* buf, size_t count);
 ssize_t ft_read(int fd, void* buf, size_t count);
 char* ft_strdup(const char* s);
+
+int	ft_atoi_base(char *str, char *base);
+
+typedef struct s_list
+{
+    void* data;
+    struct s_list* next;
+} t_list;
+
+typedef int (*t_list_cmp_data)(const void* data1, const void* data2);
+typedef int (*t_list_free_data)(void* data);
+
+void ft_list_push_front(t_list** begin_list_ptr, void* data);
+size_t ft_list_size(const t_list* begin_list);
+void ft_list_sort(t_list** begin_list_ptr, t_list_cmp_data cmp);
+void ft_list_remove_if(t_list** begin_list_ptr, void* data_ref, t_list_cmp_data cmp, t_list_free_data free_data);
