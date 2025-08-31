3d20d9c6cbe60cc9be7ee727d2d17950f294152f
jkong <jkong@student.42seoul.kr>
2025-09-01 03:54:02 +0900

    BEGIN test

---
+++ test/test.c
@@ -0,0 +1,12 @@
+#include "libasm.h"
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  (void)argc, (void)argv;
+  (void)errno;
+  (void)printf;
+  return EXIT_SUCCESS;
+}
